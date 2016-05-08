f2e2ca39f507c7cd9f5a80a30a13f6d24322ef0d
ElDanteDelAnte <sreddell@att.net>
2016-05-07 20:48:34 -0700

    Made a little progress.

--- proj5/CharListTester.java
+++ proj5/CharListTester.java
@@ -2,28 +2,28 @@ import java.io.*;
 
 public class CharListTester {
 
-	public static void main(String[] args) {
-		if (args.length != 3) {
-			System.out.println("Usage: java CharListTester <haystack_file> <searchstr_file> <method>");
-			System.exit(0);
-		}
-		
-		CharacterList haystack, searchstr;
-		int method;
-		try {
-			haystack = new CharacterList(new File(args[0]));
-			searchstr = new CharacterList(new File(args[1]));
-			method = Integer.parseInt(args[2]);
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java CharListTester <haystack_file> <searchstr_file> <method>");
+            System.exit(0);
+        }
+        
+        CharacterList haystack, searchstr;
+        int method;
+        try {
+            haystack = new CharacterList(new File(args[0]));
+            searchstr = new CharacterList(new File(args[1]));
+            method = Integer.parseInt(args[2]);
 
-			if (haystack.checkIfSubstring(searchstr, method))
-				System.out.println("true");
-			else System.out.println("false");
-		}
-		catch (IllegalArgumentException e) {
-			System.out.println("An illegal argument was passed in.");			
-		}
-		catch (IOException e) {
-			System.out.println("An IOException occurred.");
-		}
-	}
+            if (haystack.checkIfSubstring(searchstr, method))
+                System.out.println("true");
+            else System.out.println("false");
+        }
+        catch (IllegalArgumentException e) {
+            System.out.print    ln("An illegal argument was passed in.");           
+        }
+        catch (IOException e) {
+            System.out.println("An IOException occurred.");
+        }
+    }
 }
--- proj5/CharacterList.java
+++ proj5/CharacterList.java
@@ -3,121 +3,130 @@ import java.io.*;
 import java.util.*;
 
 public class CharacterList {
-	private List<Character> list; // for actual storage of characters
+    private List<Character> list; // for actual storage of characters
 
-	/* Constructors */
-	
-	public CharacterList() {
-		list = new ArrayList<Character>();
-	}
-	
-	/* This constructor makes a CharacterList out of a traditional Java string */
-	public CharacterList(String str) {
-		list = new ArrayList<Character>();
-		boolean LastCharWhiteSpace = false;
-		for (int i = 0; i < str.length(); i++) {
-			if (Character.isWhitespace(str.charAt(i))) {
-				if (!LastCharWhiteSpace) list.add(' ');
-				LastCharWhiteSpace = true;
-			}
-			else {
-				list.add(str.charAt(i));
-				LastCharWhiteSpace = false;
-			}
-		}
-	}
-	
-	/* This constructor makes a CharacterList out of the contents of a text file */
-	public CharacterList(File file) throws IOException {
-		list = new ArrayList<Character>();
-		BufferedReader reader = new BufferedReader( new InputStreamReader(new FileInputStream(file)));
-		int character;
-		boolean LastCharWhiteSpace = false;
-		while((character = reader.read()) != -1) {
-			if (Character.isWhitespace(character)) {
-				if (!LastCharWhiteSpace) list.add(' ');
-				LastCharWhiteSpace = true;
-			}
-			else {
-				list.add(new Character((char) character));
-				LastCharWhiteSpace = false;
-			}
-		}
-		if (list.get(list.size() - 1).charValue() == 32) list.remove(list.size() - 1);
-		reader.close();
-	}
+    /* Constructors */
+    
+    public CharacterList() {
+        list = new ArrayList<Character>();
+    }
+    
+    /* This constructor makes a CharacterList out of a traditional Java string */
+    public CharacterList(String str) {
+        list = new ArrayList<Character>();
+        boolean LastCharWhiteSpace = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isWhitespace(str.charAt(i))) {
+                if (!LastCharWhiteSpace) list.add(' ');
+                LastCharWhiteSpace = true;
+            }
+            else {
+                list.add(str.charAt(i));
+                LastCharWhiteSpace = false;
+            }
+        }
+    }
+    
+    /* This constructor makes a CharacterList out of the contents of a text file */
+    public CharacterList(File file) throws IOException {
+        list = new ArrayList<Character>();
+        BufferedReader reader = new BufferedReader( new InputStreamReader(new FileInputStream(file)));
+        int character;
+        boolean LastCharWhiteSpace = false;
+        while((character = reader.read()) != -1) {
+            if (Character.isWhitespace(character)) {
+                if (!LastCharWhiteSpace) list.add(' ');
+                LastCharWhiteSpace = true;
+            }
+            else {
+                list.add(new Character((char) character));
+                LastCharWhiteSpace = false;
+            }
+        }
+        if (list.get(list.size() - 1).charValue() == 32) list.remove(list.size() - 1);
+        reader.close();
+    }
 
-	
-	/* Member methods */
-	
-	/* This method returns the character at the given position in the CharacterList.
-       It should look up the appropriate character in the list used for storage */	
-	public Character getChar(int position) {
-		//TODO: Fill in code here.
-	}
+    
+    /* Member methods */
+    
+    /* This method returns the character at the given position in the Cha   racterList.
+       It should look up the appropriate character in the list used for storage */  
+    public Character getChar(int position) {
+        return list.get(position);
+    }
 
-	/* This method returns the length of the string (i.e. the size of the list) */
-	public int size() {
-		//TODO: Fill in code here.
-	}
+    /* This method returns the length of the string (i.e. the size of the list) */
+    public int size() {
+        return list.size();
+    }
+    
+    @Override
+    public int hashCode() {
+        //TODO: You must override this function and return the hash.
+    }
+    
+    public boolean checkIfSubstring(CharacterList searchstr, int method) throws IllegalArgumentException {
+        if (method < 0 || method > 1) throw new IllegalArgumentException();
+        
+        int n = list.size(); // Length of the haystack string
+        int m = searchstr.size(); // Length of the search string
+        
+        //brute force
+        if (method == 0) {
 
-	public int hashCode() {
-		//TODO: You must override this function and return the hash.
-	}
-	
-	public boolean checkIfSubstring(CharacterList searchstr, int method) throws IllegalArgumentException {
-		if (method < 0 || method > 1) throw new IllegalArgumentException();
-		
-		int n = list.size(); // Length of the haystack string
-		int m = searchstr.size(); // Length of the search string
+            //TODO: Complete the code for the brute-force method in this branch.
+            
+            // We will use a variable called "start" to keep track of the
+            // beginning position of the current portion of haystack
+            // we are comparing.
+            
+            for (int start = 0; start < n - m + 1; start++) {
 
-		if (method == 0) {
+                // If "searchstr" and the portion of haystack beginning
+                // at "start" and continuing for m characters are identical
+                // then return true.
+                for (int i = 0; i < m; i++)
+                {
+                    
+                }
 
-			//TODO: Complete the code for the brute-force method in this branch.
-			
-			// We will use a variable called "start" to keep track of the
-			// beginning position of the current portion of haystack
-			// we are comparing.
-			
-			for (int start = 0; start < n - m + 1; start++) {
+            }
+            return false; // If we get to this point, we've searched all substrings
+                          // of the haystack and haven't found a match, so return false.
+        }
+        //hashing
+        else {
 
-				// If "searchstr" and the portion of haystack beginning
-				// at "start" and continuing for m characters are identical
-				// then return true.
+            //TODO: Fill in code for the hashing method in this branch.
+            
+            // Compute the hash of the search string once.
+            
+            // Compute the starting hash of the haystack (i.e. of its first m characters)
+            
+            // If the hashes match, compare the two strings character by character.
+            // If they match char by char, return true otherwise continue further.
+            
+            // Using a loop similar to the first branch of the if statement,
+            // advance the portion of the haystack currently being examined
+            // by one character and recompute/update its hash.
+            
+            // Then compare the hashes. If they match, compare the strings
+            // character by character.
+            
+            //If they match, return true, otherwise
+            // keep going.
+            
+            return false; // If after the loop finishes and no match has been found
+                          // return false because that substring doesn't exist
+                          // in the haystack.
+        }
+    }
 
-			}
-			return false; // If we get to this point, we've searched all substrings
-			              // of the haystack and haven't found a match, so return false.
-		}
-
-		else {
-
-			//TODO: Fill in code for the hashing method in this branch.
-			
-			// Compute the hash of the search string once.
-			
-			// Compute the starting hash of the haystack (i.e. of its first m characters)
-			
-			// If the hashes match, compare the two strings character by character.
-			// If they match char by char, return true otherwise continue further.
-			
-			// Using a loop similar to the first branch of the if statement,
-			// advance the portion of the haystack currently being examined
-			// by one character and recompute/update its hash.
-			// Then compare the hashes. If they match, compare the strings
-			// character by character. If they match, return true, otherwise
-			// keep going.
-			
-			return false; // If after the loop finishes and no match has been found
-			              // return false because that substring doesn't exist
-			              // in the haystack.
-		}
-	}
-
-	/* This method prints the contents of the CharacterList. Use it for debugging. */
-	public void print() {
-		for (int i = 0; i < list.size(); i++)
-			System.out.print(list.get(i));
-		System.out.println();
-	}
+    /* This method prints the contents of the CharacterList. Use it for debugging. */
+    public void print() {
+        for (int i = 0; i < list.size(); i++)
+            System.out.print(list.get(i));
+        System.out.println();
+    }
 }
