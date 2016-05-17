093579c782bb74a310744e6a4ecf9c45b06faeba
ElDanteDelAnte <sreddell@att.net>
2016-05-17 10:00:54 -0700

    Project 6 starter kit.

---
+++ proj6/SortProblems.java
@@ -0,0 +1,28 @@
+public class SortProblems {
+
+   public static int[] IterMergeSort (int []  arr) {
+      int[] answer = new int[arr.length];
+      return answer;
+   }   //end IterMergeSort
+   
+   
+   // finds target in a two dimensional array with monotone increasing rows and columns,
+   // return {-1,-1} if not found
+   public static int[]  locator  ( int target, int [][] arr) {  
+      int[] answer = new int[2];
+      
+      //default case
+      answer[0] = -1;
+      answer[1] = -1;
+      
+      return answer;
+   }  //end locator
+   
+   
+   // Finds the number of inversions in 
+   public static int invCounter  (int [] arr ) {  
+      int answer = 1;
+      return answer;      
+   }  //end invCounter
+   
+}  // end class
---
+++ proj6/SortingAssignTester.java
@@ -0,0 +1,32 @@
+import java.util.*;  
+
+public class SortingAssignTester {
+
+      public static void main(String[] args) {
+      
+      //SortProblems sortapp = new SortProblems();
+      
+      // Test MergeSort  Test 1
+      int[] test1 = {4,  3,  2, 10, 7};      
+      int[] answer = new int[20];
+      answer = SortProblems.IterMergeSort( test1 );
+      
+
+      // Test locator  Test2  -- note haystack could be much more complex than this
+      int [][] haystack = new int[20][20];
+      for (int i = 0; i<20; i++){
+          for (int j = 0; j<20; j++){
+            haystack[i][j] = 2*i + j + 1;
+         }
+      }
+      int [] answer2 = new int[2];
+      int target = 25;
+      answer2 = SortProblems.locator(target,haystack);
+      
+      // Test invCounter  Test3 
+      
+      int answer3 = SortProblems.invCounter( test1   );
+
+
+   }
+}
