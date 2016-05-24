b96582bdfe4a14afe02685d9f146f384d1cb45c1
ElDanteDelAnte <sreddell@att.net>
2016-05-23 22:54:18 -0700

    Barely even skeletons for project 6.

---
+++ proj6/GraphDriver.java
@@ -0,0 +1,38 @@
+
+/**
+ * Test driver for the ConnectCheck and BipartiteCheck classes.
+ * 
+ * Project 6.
+ * 
+ * @author Sean Reddell
+ * @version 5/24/16
+ */
+public class GraphDriver
+{
+    /**
+     * Builds a graph from an input file and prints the results of the checks.
+     * 
+     * @param args Command-line arguments. [0] contains the filename.
+     */
+    public static void main(String[] args)
+    {
+        if (args.length != 1)
+        {
+            System.out.println("Please specify a filename");
+        }
+        else
+        {
+            /* Possibly prepare directory name */
+            
+            /* Read File */
+            
+            /* Build Graph */
+            
+            /* Test Connectivity */
+            
+            /* Test Bipartite */
+            
+            /* Print Results */
+        }
+    }
+}
