0d089f0c56d32afdeb9105a94151e667ce991cb6
ElDanteDelAnte <sreddell@att.net>
2016-05-29 22:34:09 -0700

    Combined GraphStart into TopSort.

--- proj7/TopSorter.java
+++ proj7/TopSorter.java
@@ -5,16 +5,127 @@
  * Project 7: Topological Sort
  * 
  * @author Sean Reddell
- * @version 5/26/16
+ * @version 5/29/16
  */
 
 import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.Queue;
-import java.io.IOException;
+import java.util.Scanner;
+import java.util.Collections;
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.File;
 
 public class TopSorter
 {
+    private class GraphStart
+    {
+        private int nverticies;
+        private boolean directed;
+        private LinkedList<Integer>[] edges;
+
+        /**
+        * Reads a graph file and constructs adjacency list.
+        * 
+        * @param filename Name of the file
+        * @throws FileNotFoundException if error in reading file.
+        */
+        @SuppressWarnings("unchecked")
+        private void readfile_graph(String filename) throws FileNotFoundException
+        {
+            FileInputStream in = new FileInputStream(new File(filename));
+            int x, y;
+            Scanner sc = new Scanner(in);
+            directed = (sc.nextInt() == 1); //1 directed, else undirected
+            nverticies = sc.nextInt();
+
+            //init adj lists
+            edges = new LinkedList[nverticies + 1];
+
+            for (int i = 1; i <= nverticies; i++)
+                edges[i] = new LinkedList<Integer>();
+
+            int m = sc.nextInt();
+            for (int i = 1; i <=m; i++)
+            {
+                x = sc.nextInt();
+                y = sc.nextInt();
+                insert_edge(x, y);
+            }
+        }
+
+        /**
+         * Inserts an edge into the graph.
+         * Applies to both directed and undirected graphs.
+         * 
+         * @param x Initial vertex (if directed).
+         * @param y Terminal vertex (if directed).
+         */
+        public void insert_edge(int x, int y)
+        {
+            edges[x].add(new Integer(y));
+
+            if (!directed)
+                edges[y].add(new Integer(x));
+        }
+
+        /**
+         * Removes an edge from the graph.
+         * Applies to both directed and undirected graphs.
+         * 
+         * @param x Initial vertex (if directed).
+         * @param y Terminal vertex (if directed).
+         */
+        public void remove_edge(int x, int y)
+        {
+            edges[x].remove(new Integer(y));
+
+            if (!directed)
+                edges[y].remove(new Integer(x));
+        }
+
+        /**
+         * Prints out the graph's adjacency lists.
+         */
+        public void print_graph()
+        {
+            for (int i = 1; i <= nverticies; i++)
+            {
+                System.out.print(i + ": ");
+
+                LinkedList<Integer> adj = edges[i];
+                for (Integer term : adj)
+                {
+                    System.out.print(term + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        /**
+         * Obtains the number of verticies in the graph.
+         * 
+         * @return Number of verticies.
+         */
+        public int getNVerts()
+        {
+            return nverticies;
+        }
+
+        /**
+         * Obtains the adjacency list of a vertex on the graph.
+         * 
+         * @param vertex The specified vertex.
+         * @return List of verticies adjacent to specified.
+         */
+        public LinkedList<Integer> getAdjList(int vertex)
+        {
+            return edges[vertex];
+        }
+
+    }
+    
     /**
      * Builds a graph from a file and obtains the 
      * topological ordering of its verticies.
@@ -31,12 +142,12 @@ public class TopSorter
         //build graph from file
         try
         {
-            graph = new GraphStart(filename);
+            graph.readfile_graph(filename);
             gSize = graph.getNVerts();
         }
-        catch (IOException ioe)
+        catch (FileNotFoundException fnfe)
         {
-            System.out.println("Error reading file: " + ioe);
+            System.out.println("Error reading file: " + fnfe);
         }
         catch (Exception e)
         {
